7c9ec242333b465fbff8efe3ad9228772fd4334b
FANS4ever <domciukas333@gmail.com>
2025-04-21 12:22:30 +0300

    Fix prespawned logs being broken on game start

--- FirewoodCollider/FirewoodCollider.cs
+++ FirewoodCollider/FirewoodCollider.cs
@@ -9,7 +9,7 @@ namespace FirewoodCollider
         public override string ID => "FirewoodCollider";
         public override string Name => "FirewoodCollider";
         public override string Author => "epicduck410, FANS4ever";
-        public override string Version => "2.0";
+        public override string Version => "2.1";
 
         public override void ModSetup()
         {
@@ -26,32 +26,51 @@ namespace FirewoodCollider
             var cottageLoggingCollObj = cottage.transform.Find("Logging/Pölkky/PlayerOnlyColl").gameObject;
             var cabinLoggingCollObj = cabin.transform.Find("LOD/Logging/Pölkky/PlayerOnlyColl").gameObject;
 
-            // We setup the collider here
-            var layerMask = LayerMask.NameToLayer("Collider2");
-            yardLoggingCollObj.layer = layerMask;
-            cottageLoggingCollObj.layer = layerMask;
-            cabinLoggingCollObj.layer = layerMask;
-
-            // Get wood log prefab using some sorcery
+            // Get wood log prefab used by the FSM using some sorcery
             // This is a prefab so it should affect all locations in game
             var logWall = yard.transform.Find("MachineHall/Logging/Logwall");
             PlayMakerFSM loggingwall = logWall.GetPlayMaker("Use");
             loggingwall.InitializeFSM();
             GameObject logPrefab = loggingwall.GetState("Create log").GetAction<CreateObject>(3).gameObject.Value;
 
+            // Get the prefab log half and the prespawned logs which dont get affected by prefab changes
+            var prefabLogHalf = logPrefab.transform.GetChild(0);
+            var yardLogHalf = yard.transform.Find("MachineHall/Logging/log(Clone)/log(Clone)");
+            var cottageLogHalf = cottage.transform.Find("Logging/log(Clone)/log(Clone)");
+            var cabinLogHalf = cabin.transform.Find("LOD/Logging/log(Clone)/log(Clone)");
+
             // Collider is already active so to avoid wood breaking as soon as the player 
             // presses on the log pile we make the wood unbreakable. The log collider controller
             // will change this value to the default when a new log spawns.
-            var logHalf = logPrefab.transform.GetChild(0);
-            logHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
+            prefabLogHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
+            yardLogHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
+            cottageLogHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
+            cabinLogHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
 
-            // Attach collider controller script
-            var colliderController = logHalf.gameObject.AddComponent<LogColliderController>();
-            colliderController.PushColliders = new Collider[] {
+            // We setup the collider here
+            // Do it after setting breakforce otherwise there will be one split log on the floor at each location
+            var layerMask = LayerMask.NameToLayer("Collider2");
+            yardLoggingCollObj.layer = layerMask;
+            cottageLoggingCollObj.layer = layerMask;
+            cabinLoggingCollObj.layer = layerMask;
+
+            // Attach collider controller script to wood log half
+            var prefabColliderController = prefabLogHalf.gameObject.AddComponent<LogColliderController>();
+            var yardColliderController = yardLogHalf.gameObject.AddComponent<LogColliderController>();
+            var cottageColliderController = cottageLogHalf.gameObject.AddComponent<LogColliderController>();
+            var cabinColliderController = cabinLogHalf.gameObject.AddComponent<LogColliderController>();
+
+            // Set the colliders the script will control
+            var pushColliders = new Collider[] {
                 yardLoggingCollObj.GetComponent<Collider>(),
                 cottageLoggingCollObj.GetComponent<Collider>(),
                 cabinLoggingCollObj.GetComponent<Collider>()
             };
+
+            prefabColliderController.PushColliders = pushColliders;
+            yardColliderController.PushColliders = pushColliders;
+            cottageColliderController.PushColliders = pushColliders;
+            cabinColliderController.PushColliders = pushColliders;
 
             ModConsole.Log("FirewoodCollider: Initialized!");
         }
