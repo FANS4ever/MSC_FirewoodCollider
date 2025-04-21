60e125cb6413b1dd7a5bd03cc598fe781469ee5c
FANS4ever <domciukas333@gmail.com>
2025-04-21 03:26:30 +0300

    Update mod logic. Bump version to (2.0).

---
+++ FirewoodCollider/FirewoodCollider.cs
@@ -0,0 +1,59 @@
+﻿using HutongGames.PlayMaker.Actions;
+using MSCLoader;
+using UnityEngine;
+
+namespace FirewoodCollider
+{
+    public class FirewoodCollider : Mod
+    {
+        public override string ID => "FirewoodCollider";
+        public override string Name => "FirewoodCollider";
+        public override string Author => "epicduck410, FANS4ever";
+        public override string Version => "2.0";
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+        }
+
+        private void Mod_OnLoad()
+        {
+            var yard = GameObject.Find("YARD"); // Near players house
+            var cottage = GameObject.Find("COTTAGE"); // On island
+            var cabin = GameObject.Find("CABIN"); // Ritoranta's house (Ventti guy)
+
+            var yardLoggingCollObj = yard.transform.Find("MachineHall/Logging/Pölkky/PlayerOnlyColl").gameObject;
+            var cottageLoggingCollObj = cottage.transform.Find("Logging/Pölkky/PlayerOnlyColl").gameObject;
+            var cabinLoggingCollObj = cabin.transform.Find("LOD/Logging/Pölkky/PlayerOnlyColl").gameObject;
+
+            // We setup the collider here
+            var layerMask = LayerMask.NameToLayer("Collider2");
+            yardLoggingCollObj.layer = layerMask;
+            cottageLoggingCollObj.layer = layerMask;
+            cabinLoggingCollObj.layer = layerMask;
+
+            // Get wood log prefab using some sorcery
+            // This is a prefab so it should affect all locations in game
+            var logWall = yard.transform.Find("MachineHall/Logging/Logwall");
+            PlayMakerFSM loggingwall = logWall.GetPlayMaker("Use");
+            loggingwall.InitializeFSM();
+            GameObject logPrefab = loggingwall.GetState("Create log").GetAction<CreateObject>(3).gameObject.Value;
+
+            // Collider is already active so to avoid wood breaking as soon as the player 
+            // presses on the log pile we make the wood unbreakable. The log collider controller
+            // will change this value to the default when a new log spawns.
+            var logHalf = logPrefab.transform.GetChild(0);
+            logHalf.GetComponent<FixedJoint>().breakForce = float.PositiveInfinity;
+
+            // Attach collider controller script
+            var colliderController = logHalf.gameObject.AddComponent<LogColliderController>();
+            colliderController.PushColliders = new Collider[] {
+                yardLoggingCollObj.GetComponent<Collider>(),
+                cottageLoggingCollObj.GetComponent<Collider>(),
+                cabinLoggingCollObj.GetComponent<Collider>()
+            };
+
+            ModConsole.Log("FirewoodCollider: Initialized!");
+        }
+    }
+}
---
+++ FirewoodCollider/LogColliderController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace FirewoodCollider
+{
+    public class LogColliderController : MonoBehaviour
+    {
+        public Collider[] PushColliders;
+        internal Collider logCollider;
+        internal Collider logCloneColider;
+
+        private void Start()
+        {
+            // The prefab is made of 2 parts and has 2 colliders.
+            logCollider = gameObject.transform.parent.GetComponent<Collider>();
+            logCloneColider = gameObject.transform.GetComponent<Collider>();
+
+            ignoreColliders(PushColliders, true);
+
+            // Log prefab was set to be indestructable. We ignored the collider 
+            // and the log wont break by itself so reset value to default.
+            gameObject.GetComponent<FixedJoint>().breakForce = 20000f;
+        }
+
+        private void OnJointBreak()
+        {
+            // The logs FSM script should break the log.
+            // We just need to respect the collider again.
+            ignoreColliders(PushColliders, false);
+        }
+
+        void ignoreColliders(Collider[] colliders, bool ignore)
+        {
+            for (var i = 0; i < colliders.Length; i++)
+            {
+                var collider = colliders[i];
+                Physics.IgnoreCollision(logCloneColider, collider, ignore);
+                Physics.IgnoreCollision(logCollider, collider, ignore);
+            }
+        }
+    }
+}
